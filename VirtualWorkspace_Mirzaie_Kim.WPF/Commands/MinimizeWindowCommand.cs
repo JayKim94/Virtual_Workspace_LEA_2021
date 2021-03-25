861f18aea50a3c22a7ecb453f2ba7afb3d2b7ac9
Jay Kim <jawoon.kim@edu.bib.de>
2021-03-25 11:42:08 +0100

    Added minimize window command

---
+++ VirtualWorkspace_Mirzaie_Kim.WPF/Commands/MinimizeWindowCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace VirtualWorkspace_Mirzaie_Kim.WPF.Commands
+{
+    public class MinimizeWindowCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            // Hier
+        }
+    }
+}
